2c21343f563d490b15e17b394d77949b662c78ad
Aidar <Agsafiullin@gmail.com>
2020-12-08 23:17:49 +0300

    fixed

--- src/test/java/ru/netology/domain/ProductManagerTest.java
+++ src/test/java/ru/netology/domain/ProductManagerTest.java
@@ -7,9 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProductManagerTest {
     private ProductManager productManager = new ProductManager();
-    private Book javaBook = new Book(1, "SteveJobsJavaBook", 1000, "Shildt");
+    private Book javaBook = new Book(1, "SteveJavaBook", 1000, "JobsShildt");
 //    private Book javaCode = new Book(2, "jabaCde", 1000, "Code");
-    private Smartphone iphoneX = new Smartphone(1, "SteveJobsIphoneX", 75000, "Apple");
+    private Smartphone iphoneX = new Smartphone(1, "SteveIphoneX", 75000, "JobsApple");
 //    private Smartphone iphone12 = new Smartphone(2, "iphoneX", 99000, "Apple");
 
 
@@ -26,7 +26,16 @@ class ProductManagerTest {
         productManager.add(iphoneX);
         productManager.add(javaBook);
         Product[] expected = new Product[]{iphoneX, javaBook};
-        Product[] actual = productManager.searchBy("SteveJobs");
+        Product[] actual = productManager.searchBy("Steve");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchByTxt2() {
+        productManager.add(iphoneX);
+        productManager.add(javaBook);
+        Product[] expected = new Product[]{iphoneX, javaBook};
+        Product[] actual = productManager.searchBy("Jobs");
         assertArrayEquals(expected, actual);
     }
 
